052d4f2ca795beaec7f482f8701273d7326dd90b
jouniverse <94825628+jouniverse@users.noreply.github.com>
2026-05-03 20:02:30 +0300

    Define root

--- src/next.config.js
+++ src/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/composer',
+        permanent: false,
+      },
+    ];
+  },
   images: {
     domains: [],
   },
